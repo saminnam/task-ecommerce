a136de81cf56afe60bb5bf6b0d94e8266f29ffe5
saminnam <innam8264@gmail.com>
2025-01-08 17:54:00 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,19 +4,21 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <nav className="container">
-      <img src={logo} alt="logo" width={150} height={80} />
-      <ul className="menu-list">
-        <li>Home</li>
-        <li>Products</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-      </ul>
-      <aside>
-        <AiOutlineShoppingCart className="icon" />
-        <button className="primary-btn">Sign In</button>
-      </aside>
-    </nav>
+    <div className="container">
+      <nav className="nav-bar">
+        <img src={logo} alt="logo" width={150} height={80} />
+        <ul className="menu-list">
+          <li>Home</li>
+          <li>Products</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+        <aside>
+          <AiOutlineShoppingCart className="icon" />
+          <button className="primary-btn">Sign In</button>
+        </aside>
+      </nav>
+    </div>
   );
 };
 
